bfdba1000702ba4fbaa59af0841b1c96424649f0
Abdulmjeed Bojeir <AbdulmjeedJOB12@gmail.com>
2026-02-25 01:34:44 +0300

    Bonus

---
+++ main.py
@@ -0,0 +1,14 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+    print("Addition:", x + b)
+
+try:
+    additoin(10, 20)
+
+except NameError:
+    print("Variable is not defined")
+except Exception as e:
+    print(e.__class__)
+else:
+    print("the operation is successful")
