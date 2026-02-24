d32b03fc7fb1de10296a6dd16cb66315c77fd69e
sadeem <sadeemalharbi21@example.com>
2026-02-24 20:05:57 +0300

    SADEEM'S SOLVE

---
+++ lab.py
@@ -0,0 +1,11 @@
+def additoin(x, y):
+    try:
+            x = 10
+            y = 20
+            print("Addition:", x + b)
+            print ("The operation is successful")
+    except NameError:
+            print("Error: variable is not defined")
+
+additoin(10, 20)
+
